93e4deef6bef32406ea9765e1c2ae184b9be8aa2
Aarohi Singh <aarohisingh@Aarohis-MBP.lan>
2025-11-27 19:34:44 +0530

    correcting cors config

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,16 @@ dotenv.config()
 app.use(cookieParser());
 
 
-app.use(cors({
-    origin:["https://sport-voyage.vercel.app"],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ["https://sport-voyage.vercel.app", "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 app.use(express.json())
 
