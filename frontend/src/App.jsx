0b9f52154351508d92bc1afc94f65c4924772e67
Aarohi Singh <aarohisingh@Aarohis-MBP.lan>
2025-11-27 19:22:14 +0530

    changed error pop up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
           {/* Footer always visible */}
           <Footer />
 
-          <Toaster position="top-right" />
+          <Toaster position="top-center" />
         </div>
       </AuthProvider>
     </Router>
